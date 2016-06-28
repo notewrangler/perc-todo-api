eab595e1f9e6a3d5b2105c665d299278c0c9ac7c
ralph <noiseartist@gmail.com>
2016-06-28 09:26:36 -0700

    added get reqs for todos

--- server.js
+++ server.js
@@ -2,8 +2,48 @@ var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+var todos = [
+{
+    id: 1,
+    description: 'Do Laundry',
+    completed: false
+},
+{
+    id: 2,
+    description: 'Wash Dishes',
+    completed: false
+},
+{
+    id: 3,
+    description: 'Send email to Gabe',
+    completed: true
+}
+];
+
 app.get('/', function(req, res) {
     res.send('Todo API Root');
+});
+
+app.get('/todos', function(req, res) {
+    res.json(todos);
+});
+
+app.get('/todos/:id', function(req, res) {
+    var todoId = parseInt(req.params.id, 10);
+    var matchItem;
+
+    todos.forEach( function(todo) {
+        if (todoId === todo.id) {
+            matchItem = todo;
+        }
+    });
+
+    if (matchItem) {
+        res.json(matchItem);
+    } else {
+        res.status(404).send();
+    }
+    res.send('Asking for todo with id of ' + req.params.id)
 });
 
 app.listen(PORT, function() {
