96950a8d51ea208414bee278d142a6f78de104bb
ralph <noiseartist@gmail.com>
2016-06-27 22:54:48 -0700

    3rd commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-app.get('./', function(req, res) {
+app.get('/', function(req, res) {
     res.send('Todo API Root');
 });
 
