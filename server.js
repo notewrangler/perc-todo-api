9353bdd3ed886fd91eaf8796c38754b7d073e23d
ralph <noiseartist@gmail.com>
2016-07-02 01:28:15 -0700

    Convert email to lower case

--- server.js
+++ server.js
@@ -64,6 +64,16 @@ app.post('/todos', function (req, res) {
     })
 });
 
+app.post('/users', function(req, res) {
+    var body = _.pick(req.body, 'email', 'password');
+
+    db.user.create(body).then(function(user){
+        res.json(user.toJSON());
+    }, function(e) {
+        res.status(400).json(e)
+    })
+});
+
 app.delete('/todos/:id', function(req, res) {
     var todoId = parseInt(req.params.id, 10);
 
